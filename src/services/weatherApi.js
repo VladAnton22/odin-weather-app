5ad2de752f18f6cb6c01b36f34d2f458cb495b6a
Vlad Anton <182820291+VladAnton22@users.noreply.github.com>
2025-07-05 22:09:13 +0100

    Added simple validation

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,13 +1,18 @@
 import { createGeneralForecast, createFutureForecasts } from "../models/manageForecast.js";
 
 export async function getForecastData(city, unitGroup = "metric") {
+  const error = document.getElementById("error")
   const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&key=BXBZLJM5SNLSZF5PXVAFB8ARM`, {
     mode: "cors",
   });
+  if (response.status !== 200) {
+    error.innerHTML = "City not found!";
+    return null;
+  } else {
+    const data = await response.json();
+    const generalForecast = createGeneralForecast(data);
+    const futureForecasts = createFutureForecasts(data);
 
-  const data = await response.json();
-  const generalForecast = createGeneralForecast(data);
-  const futureForecasts = createFutureForecasts(data);
-
-  return { generalForecast, futureForecasts };
+    return { generalForecast, futureForecasts };
+  }
 }
